1097fa6fed28cb21a6f0edaf669101ff59298de9
Charlot01 <justine.charlot.univ@gmail.com>
2025-05-24 14:21:16 +0200

    add script

---
+++ systeme/script.sh
@@ -0,0 +1,18 @@
+echo "Où voulez-vous enregistrer le projet ? (Chemin absolu ex : c:/dossier)"
+
+read directory
+
+echo "Quel est le nom de votre projet ?"
+
+read project
+
+cd "$directory"
+
+mkdir "$project"
+
+cd "$project"
+
+touch index.html style.css README.md
+
+echo "Le projet a été ajouté avec succès."
+
